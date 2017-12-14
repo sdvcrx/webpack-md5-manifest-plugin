a841db56a0d42b2d1bb6e41794df27dcfdf84b79
sdvcrx <memory.silentvoyage@gmail.com>
2017-12-14 20:19:27 +0800

    refactor(test): abstract test utils

---
+++ test/utils.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const path = require('path')
+const { exec } = require('child_process')
+
+const webpack = require('webpack')
+const del = require('del')
+
+const webpackConfig = require('./webpack.config')
+
+const distFolder = path.join(__dirname, '../test/dist')
+
+function getHash (filename, type = 'md5') {
+  const filePath = path.join(distFolder, filename)
+  return new Promise((resolve, reject) => {
+    exec(`${type}sum ${filePath} | cut -d ' ' -f 1`, (err, stdout) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(stdout.trim())
+    })
+  })
+}
+
+function readManifestFile (filename) {
+  const manifestPath = path.join(distFolder, filename)
+
+  return new Promise((resolve, reject) => {
+    fs.readFile(manifestPath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(JSON.parse(data))
+    })
+  })
+}
+
+function compile (plugin) {
+  const options = Object.assign({}, webpackConfig, {
+    plugins: [plugin]
+  })
+  return new Promise((resolve, reject) => {
+    webpack(options, (err, stats) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      const errors = stats.toJson().errors
+      if (errors.length > 0) {
+        reject(new Error(errors[0]))
+        return
+      }
+
+      resolve(stats)
+    })
+  })
+}
+
+function cleanup () {
+  return del(distFolder)
+}
+
+module.exports = {
+  getHash,
+  readManifestFile,
+  compile,
+  cleanup
+}
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path')
 
 const MD5Plugin = require('../')
 
-function getWebpackConfiguration (algorithm) {
-  return {
-  }
-}
-
 module.exports = {
   entry: {
     app: path.join(__dirname, './src/index.js')
